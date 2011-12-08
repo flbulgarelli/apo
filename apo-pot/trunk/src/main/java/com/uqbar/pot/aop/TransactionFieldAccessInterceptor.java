6499f2843a4f9249647603d4b2f6b235b8d15bb3
nnydjesus <nnydjesus@e1a88654-4610-4beb-b2e6-8ed2f74c6698>
2011-12-08 04:13:14 +0000

    Antes le pegaba al field cuando hacia el populate del commit, ahota invoca el setter para que el objeto tire el evento
    git-svn-id: https://xp-dev.com/svn/uqbar/projects/apo@31 e1a88654-4610-4beb-b2e6-8ed2f74c6698

--- apo-pot/trunk/src/main/java/com/uqbar/pot/aop/TransactionFieldAccessInterceptor.java
+++ apo-pot/trunk/src/main/java/com/uqbar/pot/aop/TransactionFieldAccessInterceptor.java
@@ -1,5 +1,6 @@
 package com.uqbar.pot.aop;
 
+import javassist.Modifier;
 import javassist.NotFoundException;
 import javassist.expr.FieldAccess;
 
@@ -16,11 +17,13 @@ public class TransactionFieldAccessInterceptor extends FieldAccessInterceptor {
 
 	@Override
 	protected void modifyWriterFieldAccess(StringBuffer statement, FieldAccess fieldAccess) throws NotFoundException {
-		String reemplaze = Tokens.DEFAULT_VALUE_ASSIGMENT.getRegExp();
-		
-		String newExpresion = 
-			"($fieldTypeName) $interceptor.fieldWrite($this, '$fieldName', $argument1, $this.$fieldName);";
-		statement.replace(0, statement.length(), StringUtils.replace(statement.toString(), reemplaze, newExpresion));
+		if(!Modifier.isTransient(fieldAccess.getField().getModifiers())){
+			String reemplaze = Tokens.DEFAULT_VALUE_ASSIGMENT.getRegExp();
+			
+			String newExpresion = 
+				"($fieldTypeName) $interceptor.fieldWrite($this, '$fieldName', $argument1, $this.$fieldName);";
+			statement.replace(0, statement.length(), StringUtils.replace(statement.toString(), reemplaze, newExpresion));
+		}
 	}
 
 	@Override
--- apo-pot/trunk/src/main/java/com/uqbar/pot/aop/TransactionalBehaviorAdviceWeaverStrategy.java
+++ apo-pot/trunk/src/main/java/com/uqbar/pot/aop/TransactionalBehaviorAdviceWeaverStrategy.java
@@ -8,14 +8,14 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.expr.ExprEditor;
 
+import org.uqbar.commons.utils.Transactional;
+
 import com.uqbar.commons.collections.Predicate;
 import com.uqbar.renascent.aop.pointcut.predicate.HasAnnotationPredicate;
 import com.uqbar.renascent.aop.pointcut.predicate.OrPredicate;
 import com.uqbar.renascent.aop.pointcut.predicate.SuperClassPredicate;
 import com.uqbar.renascent.framework.aop.AdviceWeaver;
-import com.uqbar.renascent.framework.aop.DirtyFlagFieldAccessInterceptor;
 import com.uqbar.renascent.framework.aop.IBehaviorAdviceWeaverStrategy;
-import com.uqbar.renascent.framework.aop.LazyRelationResolverFieldAccessInterceptor;
 import com.uqbar.renascent.framework.aop.WeavingInstrumentor;
 
 public class TransactionalBehaviorAdviceWeaverStrategy implements
@@ -24,26 +24,20 @@ public class TransactionalBehaviorAdviceWeaverStrategy implements
 	private TransactionFieldAccessInterceptor transactionInterceptor = new TransactionFieldAccessInterceptor();
 
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void addInstrumentors(ClassPool classPool,
 			Map<Predicate<CtClass>, ExprEditor> weavingInstrumentors) {
-		weavingInstrumentors.put(new SuperClassPredicate(classPool, AdviceWeaver.CLASSNAME_PERSISTIBLE),
-				new WeavingInstrumentor()
-					//transactions
-					.addFieldAccessInterceptor(transactionInterceptor));
-					// persistence
 		
 		// transactions - collections
-		weavingInstrumentors.put(new OrPredicate<CtClass>(
-			new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.Itr"),
-			new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.AbstractCollection"),
-			new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.SubListListIterator"),
-			new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.BasicList"),
-			new HasAnnotationPredicate(classPool, "org.uqbar.commons.utils.Transactional")),
+		weavingInstrumentors.put(
+			new HasAnnotationPredicate(classPool, Transactional.class.getName()),
 			new WeavingInstrumentor()
 					.addFieldAccessInterceptor(transactionInterceptor));
 
+//		new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.Itr" ),
+//		new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.AbstractCollection"),
+//		new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.SubListListIterator"),
+//		new SuperClassPredicate(classPool, "com.uqbar.renascent.common.collections.BasicList"),
 	}
 
 	@Override
