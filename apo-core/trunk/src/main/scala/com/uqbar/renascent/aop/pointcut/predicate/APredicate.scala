3ddfdd1395ad3d94e8536e6cdc980c8073c301be
nnydjesus <nnydjesus@e1a88654-4610-4beb-b2e6-8ed2f74c6698>
2012-07-19 04:54:29 +0000

    git-svn-id: https://xp-dev.com/svn/uqbar/projects/apo@222 e1a88654-4610-4beb-b2e6-8ed2f74c6698

--- apo-core/trunk/src/main/scala/com/uqbar/renascent/aop/pointcut/predicate/APredicate.scala
+++
@@ -1,8 +0,0 @@
-package com.uqbar.renascent.aop.pointcut.predicate
-import javassist.CtField
-import javassist.CtClass
-
-trait APredicate {
-  	def evaluate(ctClass:CtClass ):Boolean
-	def evaluate(ctField:CtField ):Boolean
-}
